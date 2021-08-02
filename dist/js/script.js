e6012b1ce0ae326d06d2d22cd95be45176557849
boutamente-abdessamad <boutamente.abdessamad@gmail.com>
2021-08-02 15:05:17 +0100

    init html templete starter

---
+++ dist/js/script.js
@@ -0,0 +1,44 @@
+
+/****************************************/
+/************ Main Scripts **************/
+/****************************************/
+
+$(document).ready(() => {
+
+
+    // start script 
+    try{
+
+        /* do your magic here*/
+
+        // var psItems = $('.ps-items');
+        // if (psItems.length) {
+        //     psItems.slick({
+        //         infinite: true,
+        //         speed: 300,
+        //         slidesToShow: 2,
+        //         slidesToScroll: 1,
+        //         arrows: false,
+        //         fade: false
+        //     });
+        //     $('#ps_prev').click(function (e) {
+        //         e.preventDefault();
+        //         psItems.slick('slickPrev');
+        //     });
+        //     $('#ps_next').click(function (e) {
+        //         e.preventDefault();
+        //         psItems.slick('slickNext');
+        //     });
+        // }
+
+    }catch(e){
+        console.log('Error In script detected');
+        console.log('==========================');
+        console.error(e);
+    }
+    // end script 
+
+
+
+
+})
